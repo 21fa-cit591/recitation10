57d70404a2f7ff440eaad351bd89c183d578d439
Jintong Wu <jintongw@seas.upenn.edu>
2021-11-18 00:17:53 -0500

    auto-format code

--- src/ProblemSet.java
+++ src/ProblemSet.java
@@ -4,10 +4,10 @@ import java.util.List;
 public class ProblemSet {
 
 	/**
-	 * Q1
-	 * Implement a function that return the factorial of n. You MUST use recursion.
-	 * For example, factorial(3) should yield 6 because factorial(3) = 3 * 2 * 1
-	 * Assume 100 >= n >= 1
+	 * Q1 Implement a function that return the factorial of n. You MUST use
+	 * recursion. For example, factorial(3) should yield 6 because factorial(3) = 3
+	 * * 2 * 1 Assume 100 >= n >= 1
+	 * 
 	 * @param n
 	 * @return
 	 */
@@ -16,25 +16,27 @@ public class ProblemSet {
 		return 0;
 
 	}
-	
+
 	/**
-	 * Q2
-	 * Implement a method that counts the number of 'x' in a string. You MUST use recursion.
-	 * For example, string "1x3rfdcx" has 2 'x's and countXInString("1x3rfdcx") should return 2
-	 * Assume length(input) >= 0 and all letters in input are lower-case
+	 * Q2 Given a positive integer as input, subtract another positive integer
+	 * continually until 0 or a negative value is reached, and then continually add
+	 * the second integer until the first integer is again reached. For this
+	 * question, do not end output with a newline. For example, printNumPattern(12,
+	 * 3) will output "12 9 6 3 0 3 6 9 12" to the console
+	 * 
 	 * @param input
 	 * @return
 	 */
-	public static int countXInString(String input) {
+	public static int printNumPattern(int num1, int num2) {
 		// TODO: implement this method
 		return 0;
 	}
-	
+
 	/**
-	 * Q3
-	 * Implement a method that return the reverse of the input string. You MUST use recursion.
-	 * For example, reverseString("abc") should return "cba"
-	 * Assume length(input) >= 0 and all letters in input are lower-case
+	 * Q3 Implement a method that return the reverse of the input string. You MUST
+	 * use recursion. For example, reverseString("abc") should return "cba" Assume
+	 * length(input) >= 0 and all letters in input are lower-case
+	 * 
 	 * @param input
 	 * @return
 	 */
@@ -42,14 +44,14 @@ public class ProblemSet {
 		// TODO: implement this method
 		return null;
 	}
-	
-	
+
 	/**
-	 * Q4 Challenge, Optional
-	 * Implement a method that counts the number of 1-bit in java int value. You MUST use recursion.
-	 * Given that the "int" data type in java is a 32-bit signed two's complement integer
-	 * For example, countNumberOfOneBit(5) returns 2, because the last 4 bits of the 2C representation of 5 is "0101"
-	 * Assume the input n >= 0
+	 * Q4 Challenge, Optional Implement a method that counts the number of 1-bit in
+	 * java int value. You MUST use recursion. Given that the "int" data type in
+	 * java is a 32-bit signed two's complement integer For example,
+	 * countNumberOfOneBit(5) returns 2, because the last 4 bits of the 2C
+	 * representation of 5 is "0101" Assume the input n >= 0
+	 * 
 	 * @param n
 	 * @return
 	 */
@@ -57,15 +59,13 @@ public class ProblemSet {
 		// TODO: implement this method
 		return 0;
 	}
-	
-	
-	/** 
-	 * Q5 Challenge, Optional
-	 * Implement a method that, given a list of distinct integers, 
-	 * returns lists of all possible permutations. You MUST use recursion.
-	 * For example, countNumberOfOneBit({1, 2}) returns {{1, 2}, {2, 1}}
-	 * Assume the length(input) >= 1
-	 * Hint, you might want to write a recursive helper method
+
+	/**
+	 * Q5 Challenge, Optional Implement a method that, given a list of distinct
+	 * integers, returns lists of all possible permutations. You MUST use recursion.
+	 * For example, countNumberOfOneBit({1, 2}) returns {{1, 2}, {2, 1}} Assume the
+	 * length(input) >= 1 Hint, you might want to write a recursive helper method
+	 * 
 	 * @param n
 	 * @return
 	 */
@@ -73,27 +73,24 @@ public class ProblemSet {
 		// TODO: implement this method
 		return null;
 	}
-	
-	
+
 	/*
-	 * Q6
-	 * Given an input array, shiftArray method shift the arr elements to the right by n unit
-	 * For example: shiftArray({1, 2, 3, 4, 5}, 2) returns {4, 5, 1, 2, 3}
-	 * It is guaranteed that arr.length >= 0 and n <= arr.length 
+	 * Q6 Given an input array, shiftArray method shift the arr elements to the
+	 * right by n unit For example: shiftArray({1, 2, 3, 4, 5}, 2) returns {4, 5, 1,
+	 * 2, 3} It is guaranteed that arr.length >= 0 and n <= arr.length
 	 * 
-	 * Task: Based on the D.R.Y. principle, 
-	 * improve this method to reduce the amount of repeated code 
-	 * hint: move the repeated part into a helper method
+	 * Task: Based on the D.R.Y. principle, improve this method to reduce the amount
+	 * of repeated code hint: move the repeated part into a helper method
 	 */
 	public static void shiftArray(int[] arr, int n) {
-		
+
 		// validate input
 		if (arr.length <= 1 || arr.length == n) {
 			return;
 		}
-		
+
 		int len = arr.length;
-		
+
 		// reverse the entire input array
 		int left = 0;
 		int right = len - 1;
@@ -101,10 +98,10 @@ public class ProblemSet {
 			int temp = arr[left];
 			arr[left] = arr[right];
 			arr[right] = temp;
-			left ++;
-			right --;
+			left++;
+			right--;
 		}
-		
+
 		// reverse the input array from 0 to n - 1
 		left = 0;
 		right = n - 1;
@@ -112,10 +109,10 @@ public class ProblemSet {
 			int temp = arr[left];
 			arr[left] = arr[right];
 			arr[right] = temp;
-			left ++;
-			right --;
+			left++;
+			right--;
 		}
-		
+
 		// reverse the input array from n to arr.length - 1
 		left = n;
 		right = len - 1;
@@ -123,9 +120,9 @@ public class ProblemSet {
 			int temp = arr[left];
 			arr[left] = arr[right];
 			arr[right] = temp;
-			left ++;
-			right --;
+			left++;
+			right--;
 		}
 	}
-	
+
 }
--- src/RecapExample.java
+++ src/RecapExample.java
@@ -1,23 +1,25 @@
 
 public class RecapExample {
 	/**
-	 * 1
-	 * Implement a method print the string in reverse order to the console. You MUST use recursion.
-	 * For example, printReverseString("abcd") should print "dcba\n" to the console
+	 * 1 Implement a method print the string in reverse order to the console. You
+	 * MUST use recursion. For example, printReverseString("abcd") should print
+	 * "dcba\n" to the console
+	 * 
 	 * @param n
 	 * @return
 	 */
 	public static void printReverseString(String input) {
 		// TODO: implement this method
-		// What would be the normal way to print reverse? 
+		// What would be the normal way to print reverse?
 		// How can we infer BC and sub-problem from the normal way?
 	}
+
 	/**
-	 * 2
-	 * Implement a method that counts the number of digits in java int value. You MUST use recursion.
-	 * Given that the "int" data type in java is a 32-bit signed two's complement integer
-	 * For example, countDigit(115) returns 3, because there are 3 digits in the number 115
-	 * Assume the input n > 0
+	 * 2 Implement a method that counts the number of digits in java int value. You
+	 * MUST use recursion. Given that the "int" data type in java is a 32-bit signed
+	 * two's complement integer For example, countDigit(115) returns 3, because
+	 * there are 3 digits in the number 115 Assume the input n > 0
+	 * 
 	 * @param n
 	 * @return
 	 */
@@ -25,5 +27,5 @@ public class RecapExample {
 		// TODO: implement this method
 		return 0;
 	}
-	
+
 }
